c2633d3880b8aefdce75c4c1fa9d1801770f5404
Zack Sargent <zackarywsargent@gmail.com>
2023-11-08 18:30:47 -0500

    add sample code

--- README.md
+++ README.md
@@ -1,9 +1,51 @@
 # rust-on-esp32
-Running Rust on ESP32 by the Norse IoT club
+Steps to running Rust on ESP32 by the Norse IoT club
 
-To Do:
-- create a docker container
-  - create a template directory
-  - copy all code into docekr container
-  - build container to image esp32 -> Question, will this be different across OSes?
-- all projects use this as a template from now on
+Thanks to the [esp-rust](https://github.com/esp-rs) community for making this an easy process.
+
+**These instructions are mirrored on [the wiki page](https://wiki.norseiot.club/projects/rust-on-esp32/).**
+
+----------------
+
+## Installation Instructions
+
+The following instructions were taken from <https://github.com/esp-rs/rust-build#rust-build>.
+
+## Using Rust for ESP32
+
+Step 0: install Rust & Cargo from RustUp
+- Go to <https://rustup.rs/>
+
+Step 1:  [Install espup](<https://github.com/esp-rs/rust-build#espup-installation>)
+```bash
+cargo install espup
+espup install # To install Espressif Rust ecosystem
+# [Unix]: Source the following file in every terminal before building a project
+. $HOME/export-esp.sh
+```
+Step 2: [RISC-V Installation](<https://github.com/esp-rs/rust-build#risc-v-installation>)
+
+```bash
+rustup target add riscv32imc-unknown-none-elf
+```
+Step 3: [The Cargo First Approach](<https://github.com/esp-rs/rust-build#cargo-first-approach>)
+```bash
+cargo install cargo-generate
+```
+```bash
+# STD Project
+cargo generate esp-rs/esp-idf-template cargo
+# NO-STD (Bare-metal) Project
+cargo generate esp-rs/esp-template
+```
+```bash
+cargo espflash flash <SERIAL>
+# for me, this was cargo espflash flash --port /dev/ttyUSB0 
+```
+
+# Internal Blink project
+
+The standard sanity check after creating a new project is to create a project that blinks the internal led.
+
+See [main.rs](/rust-on-esp32/src/main.rs) for the sample code.
+
--- rust-on-esp32/src/main.rs
+++ rust-on-esp32/src/main.rs
@@ -8,29 +8,30 @@ use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
 
 fn main() -> anyhow::Result<()> {
-	/* setup - runs once on board start */
-	// It is necessary to call this function once. Otherwise some patches to the runtime
+    /* setup - runs once on board start */
+    // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-	esp_idf_hal::sys::link_patches();
+    esp_idf_hal::sys::link_patches();
 
-	esp_idf_svc::log::EspLogger::initialize_default(); // initalize logger
+    esp_idf_svc::log::EspLogger::initialize_default(); // initalize logger
 
-	// the `?` at the end of the following lines means:
-	// "if this command returns an error, return the main function early with the error the function returned"
+    // The `?` at the end of the following lines means:
+    // "If this command returns an error, return the main function early with the error the function returned"
+    // It's like throwing an exception.
 
-	let peripherals = Peripherals::take()?; // get list of peripherals
-	let mut led = PinDriver::output(peripherals.pins.gpio2)?; // take the GPIO pin 2 as an output device
+    let peripherals = Peripherals::take()?; // get list of peripherals
+    let mut led = PinDriver::output(peripherals.pins.gpio2)?; // take the GPIO pin 2 as an output device
 
-	log::info!("Connected! Blinking..");
+    log::info!("Connected! Blinking..");
 
-	/* loop - equivalent to a `while(true)` loop in C++ */
-	loop {
-		led.set_high()?;
-		FreeRtos::delay_ms(1000);
+    /* loop - equivalent to a `while(true)` loop in C++ */
+    loop {
+        led.set_high()?;
+        FreeRtos::delay_ms(1000);
 
-		led.set_low()?;
-		FreeRtos::delay_ms(1000);
-	}
+        led.set_low()?;
+        FreeRtos::delay_ms(1000);
+    }
 }
 
-// run with `cargo run`
+// run with `cargo run` or `cargo espflash flash --port <SERIAL>`
